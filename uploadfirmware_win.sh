d5e3a5243cc27d191faef9a1560479f221c711f7
MBot3D <info@mbot3d.com>
2013-02-18 04:51:41 -0800

    Create uploadfirmware_win.sh
    this is upload firmware for mbot cube

---
+++ uploadfirmware_win.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# to run from the command line:   ./load_firmware_win.sh firmware_filename
+
+# default firmware_filename is mighty_one_v6.0.0.hex
+# default port is usb
+# you can change 'usb' to '/dev/ttyACM0' in linux
+
+#cd $( dirname $( readlink -f "${BASH_SOURCE[0]}" ))
+
+if test -z "$1"
+then
+  FILENAME=mighty_one_v6.0.0.hex
+else
+  FILENAME=$1
+fi
+
+while true; do
+
+FAILUSB="Fimware Program PASS"
+  
+   echo "Press Enter to upload 1280 firmware"
+   read 
+
+   # Upload firmware via usb
+   #avrdude -F -p m1280 -P $PORT -c stk500v1 -b 57600 -U flash:w:$FILENAME
+   avrdude -F -V -p m1280 -P usb -c avrispmkii -b 57600 -U flash:w:$FILENAME
+
+   if [ $? -ne 0 ]
+    then
+       FAILUSB="Fimware Program FAIL"
+    fi
+
+	echo $FAILFIRMWARE
+done
+
+#!/bin/bash
+
+
+
